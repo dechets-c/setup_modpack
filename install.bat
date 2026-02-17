9453cb7ff55df908e653fb28792f62a785c2958d
Quentin DELRIVE <136987184+dechets-c@users.noreply.github.com>
2026-02-17 16:05:30 +0100

    Improve install.bat error handling and output messages

--- install.bat
+++ install.bat
@@ -1,10 +1,24 @@
-java --version
+@echo off
+setlocal enabledelayedexpansion
+java --version > nul 2>&1
 if errorlevel 1 goto ERROR
 goto FABRIC
 
 :ERROR
-    echo "Java non installé sur la machine : installation de JDK 25"
+    echo "[!] Java non installé sur la machine : installation de JDK 25"
+    echo "[!] Tentative d'installation de JAVA"
     winget install --exact --id Oracle.JDK.25
+    echo "[!] Installation réussi. Tentative de configuration"
+    set "JAVA_HOME_TEMP=C:\Program Files\Java\jdk-25"
+    set "PATH=!JAVA_HOME_TEMP!\bin;%PATH%"
+
+    java --version > nul 2>&1
+    if errorlevel 1 (
+        echo "[!] Impossible de configurer l'environnement. Merci de relancer l'installeur"
+        goto EOERR
+    )
+    echo "[V] Installation de java réussi"
+
 
 :FABRIC    
     echo "Section Installation de Fabric"
@@ -23,27 +37,29 @@ goto FABRIC
 
     del fabric.jar
 
-    echo "Installation de Fabric réussi"
+    echo "[V] Installation de Fabric réussi"
     goto INSTALLMODS
 
 :INSTALLMODS
     echo "Téléchargement et installation des mods"
     cd "%userprofile%\AppData\Roaming\.minecraft\mods"
-    curl -O --url https://mediafilez.forgecdn.net/files/7598/646/xaerominimap-fabric-1.21.11-25.3.10.jar
-    curl -O --url https://mediafilez.forgecdn.net/files/7525/82/iris-fabric-1.10.5%2Bmc1.21.11.jar
-    curl -O --url https://cdn.modrinth.com/data/uXXizFIs/versions/Ii0gP3D8/ferritecore-8.2.0-fabric.jar
-    curl -O --url https://cdn.modrinth.com/data/AANobbMI/versions/1OWNgWVR/sodium-fabric-0.8.4%2Bmc1.21.11.jar
-    curl -O --url https://cdn.modrinth.com/data/gvQqBUqZ/versions/qvNsoO3l/lithium-fabric-0.21.3%2Bmc1.21.11.jar
-    curl -O --url https://mediafilez.forgecdn.net/files/7614/201/voicechat-fabric-1.21.11-2.6.12.jar
-    curl -O --url https://mediafilez.forgecdn.net/files/7361/977/fabric-api-0.140.2%2B1.21.11.jar
-    curl -O --url https://mediafilez.forgecdn.net/files/7598/539/xaeroworldmap-fabric-1.21.11-1.40.11.jar
+    curl -O --url "https://mediafilez.forgecdn.net/files/7598/646/xaerominimap-fabric-1.21.11-25.3.10.jar"
+    curl -O --url "https://mediafilez.forgecdn.net/files/7525/82/iris-fabric-1.10.5%2Bmc1.21.11.jar"
+    curl -O --url "https://cdn.modrinth.com/data/uXXizFIs/versions/Ii0gP3D8/ferritecore-8.2.0-fabric.jar"
+    curl -O --url "https://cdn.modrinth.com/data/AANobbMI/versions/1OWNgWVR/sodium-fabric-0.8.4%2Bmc1.21.11.jar"
+    curl -O --url "https://cdn.modrinth.com/data/gvQqBUqZ/versions/qvNsoO3l/lithium-fabric-0.21.3%2Bmc1.21.11.jar"
+    curl -O --url "https://mediafilez.forgecdn.net/files/7614/201/voicechat-fabric-1.21.11-2.6.12.jar"
+    curl -O --url "https://mediafilez.forgecdn.net/files/7361/977/fabric-api-0.140.2%2B1.21.11.jar"
+    curl -O --url "https://mediafilez.forgecdn.net/files/7598/539/xaeroworldmap-fabric-1.21.11-1.40.11.jar"
+    
+    echo "[V] Fin de l'installation des mods"
     goto EOF
 
 :EOERR
-    echo "Erreur lors de l'installation"
+    echo "[!] Erreur lors de l'installation. Envoyer moi l'erreur en privé"
     exit /b 1
 
 
 :EOF
-    echo "Installation réussi"
+    echo "[V] Installation réussi"
     exit /b 0
